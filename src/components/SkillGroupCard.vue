e302db0ad48de186eb58f8afbf034ad66c5b8dc9
Junchi <benson26365156@gmail.com>
2024-11-10 19:27:53 +0800

    feat: modify skill group card style

--- src/components/SkillGroupCard.vue
+++ src/components/SkillGroupCard.vue
@@ -3,7 +3,12 @@
     <div v-for="({ title, skills }, index) of list" :key="index" class="p-20px flex-col gap-16px">
       <p class="text-1.4rem font-700">{{ title }}</p>
       <div class="grid grid-cols-2 gap-16px">
-        <p v-for="(skill, index2) of skills" :key="index2">{{ skill }}</p>
+        <div v-for="(skill, index2) of skills" :key="index2" class="flex items-center">
+          <i class="fas fa-chevron-right text-emphasis text-1rem mr-16px" />
+          <p class="text-1.2rem inline">
+            {{ skill }}
+          </p>
+        </div>
       </div>
     </div>
   </div>
