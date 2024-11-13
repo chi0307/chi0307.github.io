008ff16118e18825153379063a8fe42938b96a20
Junchi <benson26365156@gmail.com>
2024-11-13 23:30:53 +0800

    build: add vuetify package and settings

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,24 @@
 import '@unocss/reset/tailwind.css'
 import 'virtual:uno.css'
+import 'vuetify/styles'
 
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
 
 import App from '@/App.vue'
 import router from '@/router'
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 const app = createApp(App)
-const pinia = createPinia()
 
 app.use(router)
+
+const pinia = createPinia()
 app.use(pinia)
+
+const vuetify = createVuetify({ components })
+app.use(vuetify)
 
 app.mount('#app')
