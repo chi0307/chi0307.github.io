873c803f1b0ec2ba895ab9419a0da5190558da72
Junchi <benson26365156@gmail.com>
2024-11-14 16:28:16 +0800

    feat: add exchange rate item

--- src/views/projects/AverageExchangeRate.vue
+++ src/views/projects/AverageExchangeRate.vue
@@ -10,13 +10,46 @@
       />
       <v-text-field v-model="averageRate" hide-details="auto" label="平均匯率" disabled />
     </div>
-    <v-data-table :items="columns" />
-    <div class="fixed bottom-24px right-24px">
+    <v-data-table :items="columns"> </v-data-table>
+    <div class="fixed bottom-24px right-24px" @click="showAddItemDialog = true">
       <v-btn icon>
         <i class="fas fa-plus" />
       </v-btn>
     </div>
   </main>
+  <v-dialog v-model="showAddItemDialog">
+    <v-card prepend-icon="mdi-update" title="Add Item">
+      <template #text>
+        <v-text-field
+          v-model="addItem.date"
+          type="datetime-local"
+          hide-details="auto"
+          label="交易日期"
+        />
+        <v-text-field
+          v-model.number="addItem.sell"
+          type="number"
+          hide-details="auto"
+          label="賣出台幣"
+        />
+        <v-text-field
+          v-model.number="addItem.buy"
+          type="number"
+          hide-details="auto"
+          label="買入外幣"
+        />
+        <v-text-field
+          hide-details="auto"
+          label="平均匯率"
+          :model-value="addItem.buy === 0 ? 0 : addItem.sell / addItem.buy"
+          disabled
+        />
+      </template>
+      <template #actions>
+        <v-btn class="ms-auto" text="Submit" @click="addItemEvent" />
+      </template>
+    </v-card>
+  </v-dialog>
 </template>
 <script lang="ts" setup>
 import { format } from 'date-fns'
@@ -66,6 +99,7 @@ const averageRate = computed((): number => {
 
 const transactionList = ref<AverageExchangeRateItem[]>([])
 const currentAmount = ref<number>(0)
+const showAddItemDialog = ref(false)
 
 watch(
   () => [transactionList, currentAmount],
@@ -85,4 +119,24 @@ onMounted(() => {
     currentAmount.value = restoreData.amount
   }
 })
+
+watch(showAddItemDialog, () => {
+  addItem.value = resetItem()
+})
+function resetItem(): AverageExchangeRateItem {
+  return {
+    sell: 0,
+    buy: 0,
+    date: format(new Date(), "yyyy-MM-dd'T'HH:mm")
+  }
+}
+
+const addItem = ref<AverageExchangeRateItem>(resetItem())
+
+function addItemEvent(): void {
+  const { buy, sell } = addItem.value
+  const date = new Date(addItem.value.date).toISOString()
+  transactionList.value.unshift({ buy, sell, date })
+  showAddItemDialog.value = false
+}
 </script>
