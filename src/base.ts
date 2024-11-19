43bc33ae7679e9fd41842cdf6aa00f304a482c43
Junchi <benson26365156@gmail.com>
2024-11-19 10:48:10 +0800

    refactor: themes alias

--- src/base.ts
+++ src/base.ts
@@ -3,11 +3,10 @@ import 'virtual:uno.css'
 import 'vuetify/styles'
 import '@fortawesome/fontawesome-free/css/all.css'
 
+import { colors } from '@themes/colors'
 import { createApp, type App, type Component } from 'vue'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
-
-import { colors } from '../themes/colors'
 
 export function createBaseApp(component: Component): App<Element> {
   const app = createApp(component)
