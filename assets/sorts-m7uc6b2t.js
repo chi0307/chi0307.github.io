252bbbdb1dc2db42c8bf27c4c5f1aab8d382da73
chi0307 <chi0307@users.noreply.github.com>
2024-11-19 02:11:23 +0000

    deploy: 4bd8c0f6568216da002aecf913acb760ef0eff1e

---
+++ assets/sorts-m7uc6b2t.js
@@ -0,0 +1,1 @@
+function c(s,t,a){let r;return a==="asc"?r=(u,o)=>{const e=u[t],n=o[t];return e>n?1:e<n?-1:0}:r=(u,o)=>{const e=u[t],n=o[t];return e>n?-1:e<n?1:0},[...s].sort(r)}function l(s,t,a){return c(s,t,a)}export{c as a,l as s};
