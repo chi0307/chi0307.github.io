e986e37a6aab92f6641337bec2c705e5d8fdb28f
Junchi <benson26365156@gmail.com>
2024-12-14 15:10:07 +0800

    feat: add DateISOString typing

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,6 +2,7 @@ import type { AllowedComponentProps, Component } from 'vue'
 
 export type URL = `https://${string}`
 export type UUID = `${string}-${string}-${string}-${string}-${string}`
+export type DateISOString = `${number}-${number}-${number}T${number}:${number}:${number}.${number}Z`
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type PropTypeOf<C extends Component> = C extends new (...args: any) => any
