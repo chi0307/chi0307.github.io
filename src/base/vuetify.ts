5a05d808056e20d357392608f72656117bdc2ed9
Junchi <benson26365156@gmail.com>
2024-12-14 19:25:47 +0800

    feat: add material design icons

--- src/base/vuetify.ts
+++ src/base/vuetify.ts
@@ -1,9 +1,11 @@
+import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 
 import { colors } from '@themes/colors'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import { aliases, fa } from 'vuetify/iconsets/fa'
+import { mdi } from 'vuetify/iconsets/mdi'
 
 export const vuetify = createVuetify({
   components,
@@ -20,6 +22,7 @@ export const vuetify = createVuetify({
     aliases,
     sets: {
       fa,
+      mdi,
     },
   },
 })
