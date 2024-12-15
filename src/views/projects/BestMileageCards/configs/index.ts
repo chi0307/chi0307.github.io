45af6c35cb6871a82f4ab53776992f90bfb9275d
Junchi <benson26365156@gmail.com>
2024-12-15 12:22:07 +0800

    config: add hsbc, cube card config

--- src/views/projects/BestMileageCards/configs/index.ts
+++ src/views/projects/BestMileageCards/configs/index.ts
@@ -1,5 +1,9 @@
+import { isNotNullishValue } from '@/utils'
+
 import type { CardConfig } from '../CreditCard'
 import { cubeConfigs } from './cathay'
 import { travelConfigs } from './hsbc'
 
-export const defaultCardConfigs: CardConfig[] = [...travelConfigs, ...cubeConfigs]
+export const defaultCardConfigs: CardConfig[] = [travelConfigs[2], cubeConfigs[4]].filter(
+  isNotNullishValue,
+)
