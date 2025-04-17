0c46c97a7fd4271576eb3069d099f1b13dbd26b9
Junchi <benson26365156@gmail.com>
2025-04-17 22:56:50 +0800

    feat: GA base setting

--- src/base/index.ts
+++ src/base/index.ts
@@ -1,3 +1,17 @@
+const script1 = document.createElement('script')
+script1.src = 'https://www.googletagmanager.com/gtag/js?id=G-43L9SJJ7CJ'
+script1.async = true
+document.head.appendChild(script1)
+
+const script2 = document.createElement('script')
+script2.innerHTML = `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-43L9SJJ7CJ');
+`
+document.head.appendChild(script2)
+
 import '@unocss/reset/tailwind.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 import 'virtual:uno.css'
